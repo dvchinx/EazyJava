02caef0f2d0bd4d6b6d4fbb2223add5e3c96ea55
Luis Ernesto Bueno Solano <luisbuemaxx@gmail.com>
2025-10-04 18:06:50 +0000

    Añadir el metodo println

--- src/main/java/org/agusdevs/Main.java
+++ src/main/java/org/agusdevs/Main.java
@@ -3,54 +3,90 @@ package org.agusdevs;
 public class Main {
 
     public static void main(String[] args) {
-        print("Hola mundo");
+        print("Hola");
     }
-
     public static void print(String txt) {
+        try {
+            System.out.print(txt);
+        } catch (Exception e) {
+            System.out.println("Error (String): " + e.getMessage());
+        }
+    }
+    public static void print(int num) {
+        try {
+            System.out.print(num);
+        } catch (Exception e) {
+            System.out.println("Error (int): " + e.getMessage());
+        }
+    }
+    public static void print(long num) {
+        try {
+            System.out.print(num);
+        } catch (Exception e) {
+            System.out.println("Error (long): " + e.getMessage());
+        }
+    }
+    public static void print(double num) {
+        try {
+            System.out.print(num);
+        } catch (Exception e) {
+            System.out.println("Error (double): " + e.getMessage());
+        }
+    }
+    public static void print(boolean bool) {
+        try {
+            System.out.print(bool);
+        } catch (Exception e) {
+            System.out.println("Error (boolean): " + e.getMessage());
+        }
+    }
+    public static void print(char ch) {
+        try {
+            System.out.print(ch);
+        } catch (Exception e) {
+            System.out.println("Error (char): " + e.getMessage());
+        }
+    }
+    public static void println(String txt) {
         try {
             System.out.println(txt);
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error (String): " + e.getMessage());
         }
     }
-
-    public static void print(Integer num) {
+    public static void println(int num) {
         try {
             System.out.println(num);
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error (int): " + e.getMessage());
         }
     }
-
-    public static void print(Long num) {
+    public static void println(long num) {
         try {
             System.out.println(num);
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error (long): " + e.getMessage());
         }
     }
-
-    public static void print(Double num) {
+    public static void println(double num) {
         try {
             System.out.println(num);
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error (double): " + e.getMessage());
         }
     }
-
-    public static void print(Boolean bool) {
+    public static void println(boolean bool) {
         try {
             System.out.println(bool);
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error (boolean): " + e.getMessage());
         }
     }
-
-    public static void print(char caracter) {
+    public static void println(char ch) {
         try {
-            System.out.println(caracter);
+            System.out.println(ch);
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error (char): " + e.getMessage());
         }
     }
 }
