4c13799b0e1e3746e3a4637be743274102084311
Luis Ernesto Bueno Solano <luisbuemaxx@gmail.com>
2025-10-04 12:46:31 -0500

    Update Main.java with Print

--- src/main/java/org/agusdevs/Main.java
+++ src/main/java/org/agusdevs/Main.java
@@ -1,8 +1,56 @@
 package org.agusdevs;
 
 public class Main {
+
     public static void main(String[] args) {
+        print("Hola mundo");
+    }
 
+    public static void print(String txt) {
+        try {
+            System.out.println(txt);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 
+    public static void print(Integer txt) {
+        try {
+            System.out.println(txt);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void print(Long txt) {
+        try {
+            System.out.println(txt);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void print(Double txt) {
+        try {
+            System.out.println(txt);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void print(Boolean txt) {
+        try {
+            System.out.println(txt);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void print(char txt) {
+        try {
+            System.out.println(txt);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
